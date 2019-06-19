f9e2d500b43692f53170761ec11da6e70d796702
piokowalski <piokowalski@onet.pl>
2019-06-19 17:49:32 +0200

    Python basic syntax - while loop - simple password checker

--- Basics.py
+++ Basics.py
@@ -1,6 +1,15 @@
 
-x = 3
+x = 7
 
 while x < 10:
     print("Smaller than ten")
     x = x + 2
+
+
+    correct_password = "aezakmi"
+password = input("Enter the password: ")
+
+while correct_password != password:
+    password = input("Try again, password: ")
+
+    print("You're logged in.")
