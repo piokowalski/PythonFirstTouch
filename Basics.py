11b9c00bd305b8e1ffc1d1f94e8c70f8f2f24da5
piokowalski <piokowalski@onet.pl>
2019-06-19 18:09:26 +0200

    Python basic syntax - String formatting - using ( )

--- Basics.py
+++ Basics.py
@@ -5,5 +5,5 @@ password = input("Enter the password: ")
 while correct_password != password:
     password = input("Try again, password: ")
 
-message = "Hello, %s, You're logged in." % personName
+message = ("Hello,",personName,"You're logged in.") # Printing as array
 print(message)
