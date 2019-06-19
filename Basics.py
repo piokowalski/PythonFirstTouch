6d7f1d87af3b8166b14c8e1e8f23691e49862350
piokowalski <piokowalski@onet.pl>
2019-06-19 17:59:19 +0200

    Python basic syntax - while loop - String used

--- Basics.py
+++ Basics.py
@@ -1,15 +1,8 @@
-
-x = 7
-
-while x < 10:
-    print("Smaller than ten")
-    x = x + 2
-
-
-    correct_password = "aezakmi"
+correct_password = "aezakmi"
+personName = input("Enter Your name: ")
 password = input("Enter the password: ")
 
 while correct_password != password:
     password = input("Try again, password: ")
 
-    print("You're logged in.")
+print("Hello",personName,"You're logged in.")
