4eeff571cde558b8a3e2098d2a45c7329b31e1bc
piokowalski <piokowalski@onet.pl>
2019-06-20 10:13:08 +0200

    Python basic syntax - Arrays - basic printing operations

--- Arrays.py
+++ Arrays.py
@@ -1,2 +1,9 @@
 family = ["Mutter", "Vater", "Tante", "Schwester"]
 print(family)
+# printing all array
+
+print(family[2:])
+# printing members from index 2 and higher
+
+print(family[0:2])
+# printing members from index 0 to 1 ( x<2 )
