7eb8b93cf689f7b33523f598bc533c95c85fcf65
piokowalski <piokowalski@onet.pl>
2019-06-20 10:20:49 +0200

    Python basic syntax - Arrays - adding, and removing array elements

--- Arrays.py
+++ Arrays.py
@@ -7,3 +7,16 @@ print(family[2:])
 
 print(family[0:2])
 # printing members from index 0 to 1 ( x<2 )
+
+print("\n")
+
+numbers = [12, 52, -4, 9.4, 999]
+family.extend(numbers)
+print(family)
+#Extending one array with another
+
+family.append("Oma")
+family.insert(1, "Opa")
+family.remove("Tante")
+print(family)
+#Adding new single element "Oma" in the end, and "Opa" at 2nd index, and removing "Tante"
