50f2f79548ac34685775d484a818ac391f75fd4d
piokowalski <piokowalski@onet.pl>
2019-06-20 09:35:31 +0200

    Python basic syntax - String - method len - counting chars

--- Basics.py
+++ Basics.py
@@ -5,5 +5,8 @@ password = input("Enter the password: ")
 while correct_password != password:
     password = input("Try again, password: ")
 
-message = ("Hello,",personName,"You're logged in.") # Printing as array
+message = ("Hello %s, You're logged in." %personName)
 print(message)
+
+print(len(message))
+# counting letters
