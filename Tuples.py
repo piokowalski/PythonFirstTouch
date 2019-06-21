bd965bbf18d75ffcfab72a43deefa5a962103b3f
piokowalski <piokowalski@onet.pl>
2019-06-21 13:29:19 +0200

    Python basic syntax - Tuples - basics

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+# tuple example (they're immutable)
+
+coordinates = (5,8)
+
+print(coordinates[0])
+#printing value of index 0
+
+# coordinates[1] = 9
+# print(coordinates) #I told You, immutable ;)! Created once, ready to use
+
