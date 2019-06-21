0ca6c091132124ec1146faa6e04c683bfa5b25ad
piokowalski <piokowalski@onet.pl>
2019-06-21 13:34:43 +0200

    Python basic syntax - Tuples - list of tuples

--- Tuples.py
+++ Tuples.py
@@ -6,5 +6,8 @@ print(coordinates[0])
 #printing value of index 0
 
 # coordinates[1] = 9
-# print(coordinates) #I told You, immutable ;)! Created once, ready to use
+print(coordinates) #I told You, immutable ;)! Created once, ready to use
+# It's like a list[]
 
+tuplesList = [(1,4), (3,9), (8,8)]
+print(tuplesList)
