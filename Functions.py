89ac9e1a09faf95566c66151ea3f2ebd7d16887b
piokowalski <piokowalski@onet.pl>
2019-06-21 13:54:30 +0200

    Python basic syntax - Functions - adding parameters

--- Functions.py
+++ Functions.py
@@ -7,3 +7,16 @@ def say_hi():
      print("Hello %s !" %user)
 
 say_hi()
+print("\n")
+
+
+
+#adding values into functions (like in JAVA)
+
+def user_logged(name):
+     print(name +" logged in.")
+
+
+user_logged("Administrator")
+user_logged("User")
+
