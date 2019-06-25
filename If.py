38ce9c0515ced33670a2e44dff2cc8b6305973e4
piokowalski <piokowalski@onet.pl>
2019-06-25 17:10:53 +0200

    If statements - using in function

--- If.py
+++ If.py
@@ -40,3 +40,13 @@ if is_boy or is_tall:
     print("Eligible.")
 else:
     print("Denied.")
+
+def max_num (num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(3, 4, 5))
