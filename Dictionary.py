be47ef7c37cd9fec4e956dcba0448d6896f28367
piokowalski <piokowalski@onet.pl>
2019-06-26 17:15:22 +0200

    Dictionary - created pairs <key, value>

---
+++ Dictionary.py
@@ -0,0 +1,17 @@
+
+monthConversion = {
+    "Jan":"January",
+    "Feb":"February",
+    "Mar":"March",
+    "Apr":"April",
+    "May":"May",
+    "Jun":"June",
+    "Jul":"July",
+    "Aug":"August",
+    "Sep":"September",
+    "Oct":"October",
+    "Nov":"November",
+    "Dec":"December"
+}
+
+print(monthConversion.get("Jan"))
