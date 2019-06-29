19f3c6bd8b4b187b700fe3f32790e0402b9ecce2
piokowalski <piokowalski@onet.pl>
2019-06-29 09:51:29 +0200

    Dictionary - simple translator changing vowels into "x"

--- Dictionary.py
+++ Dictionary.py
@@ -15,3 +15,16 @@ monthConversion = {
 }
 
 print(monthConversion.get("Jan"))
+
+#Simple translation
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "aeiouAEIOU":
+            translation = translation + "x"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter your word: ")))
