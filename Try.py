9e243a1b5ae374ac913246d1e2abfd71998bf349
piokowalski <piokowalski@onet.pl>
2019-06-29 10:10:02 +0200

    Try (catch) - specified exceptions

--- Try.py
+++ Try.py
@@ -4,3 +4,13 @@ try:
     print(number)
 except:
     print("Input is invalid!")
+
+
+try:
+    value = 10/0
+    digit = int(input("Digit please: "))
+    print(digit)
+except ZeroDivisionError:
+    print("Divided by zero, cholero")
+except ValueError:
+    print("Invalid digit!")
