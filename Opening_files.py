1dd7494e5512ee6538743ccabc8c4533365355d6
piokowalski <piokowalski@onet.pl>
2019-06-30 10:25:30 +0200

    Opening files in Python - reading document with "r" mode

--- Opening_files.py
+++ Opening_files.py
@@ -1,7 +1,7 @@
 #opening files in Python
 
-counting_file = open("Open_File.txt", "r ")
-
-print(counting_file.readable())
+counting_file = open("Open_File.txt", "r")
+# r - read, w - write, r+ - read/write, a - adding new lines in the file
+print(counting_file.read())
 
 counting_file.close()
