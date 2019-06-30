908763c243a07cc62efe4e150db0dff8f3ec912d
piokowalski <piokowalski@onet.pl>
2019-06-30 10:32:37 +0200

    Opening files in Python - printing specific line of the file

--- Opening_files.py
+++ Opening_files.py
@@ -4,8 +4,10 @@ counting_file = open("Open_File.txt", "r")
 # r - read, w - write, r+ - read/write, a - adding new lines in the file
 # print(counting_file.read())
 
-print(counting_file.readline())
-print(counting_file.readline())
+# print(counting_file.readline())
+# print(counting_file.readline())
 
-print(counting_file.readlines())
+# print(counting_file.readline()[0])
+print(counting_file.readlines()[0])
+
 counting_file.close()
