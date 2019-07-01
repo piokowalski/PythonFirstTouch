072e99edc3f7b423a93de39457b0bbf00f69a578
piokowalski <piokowalski@onet.pl>
2019-07-01 09:26:28 +0200

    Importing - created new methods in Functions.py to import it

--- Functions.py
+++ Functions.py
@@ -1,5 +1,5 @@
-
 # keyword in Python = def
+import random
 
 user = "Jonasz"
 
@@ -9,24 +9,17 @@ def say_hi():
 say_hi()
 print("\n")
 
-
-
 #adding values into functions (like in JAVA)
-
 def user_logged(name):
      print(name +" logged in.")
-
 
 user_logged("Administrator")
 user_logged("User")
 
-
 # return in functions
-
 def cube(numba):
      return numba*numba*numba
      print("It's not going to be printed - must be before return!")
-
 
 print(cube(4))
 #Returning cube of number
@@ -34,3 +27,12 @@ print(cube(4))
 result = cube(5)
 print(result)
 #other method to call this function
+
+#to_import
+def roll_dice(num):
+     return random.randint(1, num)
+
+
+feet_in_mile = 5280
+meters_in_km = 1000
+ducks = ["Dyzio", "Hyzio", "Zyzio"]
