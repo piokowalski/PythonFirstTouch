9806806b9b490d35154148fa97aecccf64c423ab
piokowalski <piokowalski@onet.pl>
2019-07-01 09:17:41 +0200

    Opening files in Python - writing to the new file

--- Opening_files.py
+++ Opening_files.py
@@ -13,5 +13,11 @@ counting_file = open("Open_File.txt", "r")
 for line in counting_file.readlines():
     print(line)
 
-
 counting_file.close()
+
+#Writing to the file - "a" appending
+write_file = open("Opening_files_writing","a")
+
+write_file.write("This is a new line created by python, tsssss.")
+
+write_file.close()
