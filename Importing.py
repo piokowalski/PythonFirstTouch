ad3b35e5233421fefdec4e3e9f01c588fbb9c569
piokowalski <piokowalski@onet.pl>
2019-07-01 09:38:24 +0200

    Importing - taking methods from other files / internal python modules

--- Importing.py
+++ Importing.py
@@ -1,6 +1,15 @@
 import Functions
+import abc
+
 
 two_random_rolls = Functions.roll_dice(2)
 print(two_random_rolls)
 
 print(Functions.roll_dice(5))
+print(Functions.ducks)
+print(Functions.feet_in_mile*2)
+
+# Importing from Python modules
+
+abc._abc_init("a")
+
