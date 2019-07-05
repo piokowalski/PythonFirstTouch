f4ebdbf454c183e29688f18c9835c4773e23018f
piokowalski <piokowalski@onet.pl>
2019-07-05 17:49:18 +0200

    Object functions - calling function on created object

--- Student.py
+++ Student.py
@@ -7,3 +7,5 @@ class Student:
     def on_honor_roll(self):
         if self.gpa >= 3.5:
             return True
+        else:
+            return False
--- Student_functions.py
+++ Student_functions.py
@@ -2,3 +2,5 @@ from Student import Student
 
 student1 = Student("Michael", "Computer science", 3.2)
 student2 = Student("Pamela", "Art", 3.9)
+
+print(student1.on_honor_roll())
